adbdb19de67bcef555581e7e16edd73f06b1ee4c
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-06-30 13:53:32 +0100

    chatbot functions. started customization. need to integrate backend iinto prompts and improve aesthetics

--- Frontend/camera_app/src/components/chatbot.jsx
+++ Frontend/camera_app/src/components/chatbot.jsx
@@ -1,10 +1,30 @@
 import { useState } from "react";
 import { json } from "react-router-dom";
 
+const GetData = () => {};
+
 const Chatbot = () => {
+  //const initialHistory =
   const [value, setValue] = useState("");
   const [error, setError] = useState("");
-  const [chatHistory, setChatHistory] = useState([]);
+  const [chatHistory, setChatHistory] = useState([
+    {
+      role: "user",
+      parts: [
+        {
+          text: "You are a virtual assistant 'Gary' in the online second hand camera store 'camera store'. Your aim is to answer customer`s questions about different cameras. You are to recommend camera`s based on suitability, and inform customers which cameras are currently available. You are to use relatively short replies with friendly and informal language.",
+        },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
+        {
+          text: "Hello. I'm Gary, your virtual assistant at the 'Camera Store'! What can I help you with today? 😊\n",
+        },
+      ],
+    },
+  ]);
 
   const getResponse = async () => {
     if (!value) {
@@ -30,11 +50,11 @@ const Chatbot = () => {
         ...oldChatHistory,
         {
           role: "user",
-          parts: [value],
+          parts: [{ text: value }],
         },
         {
           role: "model",
-          parts: [data],
+          parts: [{ text: data }],
         },
       ]);
       setValue("");
@@ -50,6 +70,8 @@ const Chatbot = () => {
     setError("");
     setChatHistory([]);
   };
+
+  let tracker = 0;
   return (
     <>
       <div id="chatbot">
@@ -61,14 +83,22 @@ const Chatbot = () => {
         />
       </div>
       {!error && <button onClick={getResponse}>Ask</button>}
-      {error && <button onClick={clear}>Clear</button>}
+      {error && (
+        <div>
+          <button onClick={clear}>Clear</button>
+          <button onClick={getResponse}>Ask</button>
+        </div>
+      )}
       {error && <p>{error}</p>}
       <div className="searchResult">
         {chatHistory.map((chat_line, index) => (
           <div key={index}>
-            <p>
-              {chat_line.role} : {chat_line.parts}
-            </p>
+            {index >= 1 && (
+              <p>
+                {chat_line.role === "model" ? "Gary" : "Me"} :{" "}
+                {chat_line.parts[0].text}
+              </p>
+            )}
           </div>
         ))}
       </div>
