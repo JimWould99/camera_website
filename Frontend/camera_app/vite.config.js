290acb40ee667846650b8541395ed766cfedc0c4
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-07-10 13:13:21 +0100

    deployment changes

--- Frontend/camera_app/vite.config.js
+++ Frontend/camera_app/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:2002",
+        target: "https://camera-website-backend.onrender.com",
         changeOrigin: true,
         secure: false,
       },
