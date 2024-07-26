72d63e2ae6e9bde0e8606e0cd6a7510c0b3a2e91
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-07-26 18:22:57 +0100

    another method

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,14 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+//app.use(cors());
 /*app.use(
   cors({
     origin: "https://camera-website-frontend.onrender.com",
