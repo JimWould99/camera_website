ae6b3b9663fd77276da7eedeef2dbe64065bdb93
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-08-28 10:42:54 +0100

    first deployment attempt

--- Backend/app.js
+++ Backend/app.js
@@ -3,21 +3,37 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
 
-const indexRouter = require("./routes/index");
-const productRouter = require("./routes/product");
-const userRouter = require("./routes/users");
+/*app.use(
+  cors({
+    origin: "*",
+  })
+);*/
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+//app.use(cors());
+/*app.use(
+  cors({
+    origin: "https://camera-website-frontend.onrender.com",
+  })
+); */
 
 //app.use(express.static("dist"));
 
 app.use(express.json());
 
-/*app.use(
-  cors({
-    origin: "https://camera-website-frontend.onrender.com",
-  })
-);*/
+//app.use(cors());
+
+const indexRouter = require("./routes/index");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/users");
 
 mongoose
   .connect(process.env.MONGO_URI)
